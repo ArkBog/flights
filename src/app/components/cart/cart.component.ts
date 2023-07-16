72ad5b15a8d5d1353e3341734cb1df20a4bb059c
ArkBog <arkadiuszbogdanski1@gmail.com>
2023-07-16 15:59:11 +0200

    add discount for child

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -5,36 +5,44 @@ import { Ticket } from 'src/app/interfaces/interfaces';
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.scss']
+  styleUrls: ['./cart.component.scss'],
 })
 export class CartComponent implements OnInit {
+  basket: Ticket[] = [];
 
-  basket:Ticket[] = [];
+  totalCost: any[] = [];
+  sum: any;
 
-  totalCost:any[] = [];
-  sum:any;
-  
-  constructor(private basketService: BasketService){}
+  constructor(private basketService: BasketService) {}
 
   ngOnInit(): void {
     this.totalCost = [];
     this.basket = [];
     console.log(this.totalCost);
     this.basket = this.basketService.basketFromService;
+    console.log(this.basket);
 
-    let ticketCost:number;
+    let ticketCost: number;
 
-    
-    for (let i = 0; i < this.basket.length; i++){
+    for (let i = 0; i < this.basket.length; i++) {
+      // let birthdate = new Date(this.basket[i].dateOfBirth)
+      let timeDiff = Math.abs(
+        Date.now() - new Date(this.basket[i].dateOfBirth).getTime()
+      );
+      let age = Math.floor(timeDiff / (1000 * 3600 * 24) / 365.25);
+      console.log(age);
       ticketCost = this.basket[i].price;
-      if(this.basket[i].twoWayTravel === true){
-        ticketCost = ticketCost * 2
-      };
-      if(this.basket[i].additionalLuggage === true){
-        ticketCost = ticketCost + 50
-      };
-      
-      this.totalCost.push(ticketCost)
+      if (age < 8) {
+        ticketCost = ticketCost * 0.5;
+      }
+      if (this.basket[i].twoWayTravel === true) {
+        ticketCost = ticketCost * 2;
+      }
+      if (this.basket[i].additionalLuggage === true) {
+        ticketCost = ticketCost + 50;
+      }
+
+      this.totalCost.push(ticketCost);
     }
 
     console.log(this.totalCost);
--- src/app/components/flight-details/flight-details.component.ts
+++ src/app/components/flight-details/flight-details.component.ts
@@ -6,8 +6,9 @@ import { SeatsService } from 'src/app/services/seats.service';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Ticket } from 'src/app/interfaces/interfaces';
 import { BasketService } from 'src/app/services/basket.service';
+import { DatePipe } from '@angular/common';
 
-const airports = (<any>data);
+const airports = <any>data;
 
 @Component({
   selector: 'app-flight-details',
@@ -16,108 +17,119 @@ const airports = (<any>data);
 })
 export class FlightDetailsComponent implements OnInit {
   constructor(
+    public datepipe: DatePipe,
     private userIsLoggedInfo: UserIsLoggedService,
     private seatsService: SeatsService,
-    private basketService: BasketService,
+    private basketService: BasketService
   ) {}
 
   airports: any = airports.default;
 
-  numberOfPassangersFromLocal:any;
-  numberOfPassangersArray:any = [];
+  today: Date = new Date();
+  maxDate: any;
+
+  numberOfPassangersFromLocal: any;
+  numberOfPassangersArray: any = [];
 
   userIsLoggedStatus!: boolean;
-  flightsForm:any;
-  storageForm:any;
+  flightsForm: any;
+  storageForm: any;
 
-  startFly:any;
-  convertFlyFromToNumber:any;
+  startFly: any;
+  convertFlyFromToNumber: any;
   startFlyHeader!: string;
 
-  endFly:any;
-  convertFlyToToNumber:any;
+  endFly: any;
+  convertFlyToToNumber: any;
   endFlyHeader!: string;
 
   departureDate!: string;
   returnDate!: string;
-  
+
   seats!: number;
 
   status!: string;
 
-  yourSeat!:string
+  yourSeat!: string;
 
-  yourSeats:string[] = [];
+  yourSeats: string[] = [];
 
-  price!:number;
+  price!: number;
 
-  twoWayTravel!:boolean;
+  twoWayTravel!: boolean;
 
-  bookedTicket:any;
-  basket:Ticket[] = [];
+  bookedTicket: any;
+  basket: Ticket[] = [];
+
+  extraLuggage: boolean = false;
 
   ticket = new FormGroup({
     name: new FormControl(),
     surname: new FormControl(),
     dateOfBirth: new FormControl(),
-    additionalLuggage: new FormControl(this.yourSeat),
+    additionalLuggage: new FormControl(),
     seat: new FormControl(),
-  })
+  });
 
   ngOnInit(): void {
-      this.userIsLoggedInfo.userIsLogged.subscribe(userIsLoggedStatus => this.userIsLoggedStatus = userIsLoggedStatus);
+    this.maxDate = this.datepipe.transform(this.today, 'yyyy-MM-dd');
 
+    this.userIsLoggedInfo.userIsLogged.subscribe(
+      (userIsLoggedStatus) => (this.userIsLoggedStatus = userIsLoggedStatus)
+    );
 
-    this.storageForm = localStorage.getItem("flyFrom");
+    this.storageForm = localStorage.getItem('flyFrom');
 
     this.flightsForm = JSON.parse(this.storageForm);
     this.convertFlyFromToNumber = Number(this.flightsForm.flyFrom);
-    
+
     this.startFly = this.airports[this.convertFlyFromToNumber];
-    console.log(this.startFly)
+    console.log(this.startFly);
     this.startFlyHeader = this.startFly.from;
 
     this.convertFlyToToNumber = Number(this.flightsForm.flyTo);
     console.log(this.convertFlyFromToNumber);
     this.endFly = this.startFly.departures[this.convertFlyToToNumber];
     this.endFlyHeader = this.endFly.name;
-    
 
     this.departureDate = `${this.flightsForm.departureDate} ${this.endFly.startTime}`;
-    this.returnDate = `${this.flightsForm.returnDate} ${this.airports[this.convertFlyFromToNumber].departures[this.convertFlyToToNumber].startTime}`;
+    this.returnDate = `${this.flightsForm.returnDate} ${
+      this.airports[this.convertFlyFromToNumber].departures[
+        this.convertFlyToToNumber
+      ].startTime
+    }`;
     this.seats = this.startFly.departures[this.convertFlyToToNumber].seats;
     this.seatsService.seats = this.seats;
 
-    this.price = this.airports[this.convertFlyFromToNumber].departures[this.convertFlyToToNumber].price;
+    this.price =
+      this.airports[this.convertFlyFromToNumber].departures[
+        this.convertFlyToToNumber
+      ].price;
 
     this.numberOfPassangersFromLocal = this.flightsForm.numberOfPassangers;
 
-
-    for(let i = 0; i < this.numberOfPassangersFromLocal; i++){
-      this.numberOfPassangersArray.push(i)
+    for (let i = 0; i < this.numberOfPassangersFromLocal; i++) {
+      this.numberOfPassangersArray.push(i);
     }
 
-    this.seatsService.currentStatus.subscribe(status => this.status = status);
-
+    this.seatsService.currentStatus.subscribe(
+      (status) => (this.status = status)
+    );
   }
 
-  reciveData($event: any){
+  reciveData($event: any) {
     this.yourSeat = $event;
     this.yourSeats.push(this.yourSeat);
   }
 
-  displayComponentChooseSeat(){
-    this.seatsService.changeStatus("block")
+  displayComponentChooseSeat() {
+    this.seatsService.changeStatus('block');
   }
 
-  
-  onSubmitTicket(param:any){
-    if(this.flightsForm.returnDate === null){
-      this.twoWayTravel = false
-    }
-    else(
-      this.twoWayTravel = true
-    )
+  onSubmitTicket(param: any) {
+    if (this.flightsForm.returnDate === null) {
+      this.twoWayTravel = false;
+    } else this.twoWayTravel = true;
     this.bookedTicket = {
       name: this.ticket.value.name,
       surname: this.ticket.value.surname,
@@ -132,5 +144,8 @@ export class FlightDetailsComponent implements OnInit {
     this.yourSeats.splice(param, 1);
     this.numberOfPassangersArray.splice(param, 1);
     console.log(this.numberOfPassangersArray.length);
+  }
+  extraLuggageStatus(param: any) {
+    this.extraLuggage = param;
   }
 }
